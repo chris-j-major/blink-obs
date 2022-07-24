801739c894d60f869b301e71fe51249a5731099f
Chris Major <chris.j.majoir@gmail.com>
2022-07-24 13:02:50 +0100

    Fix play forever flag

--- src/main/java/com/thingm/blink1/Blink1.java
+++ src/main/java/com/thingm/blink1/Blink1.java
@@ -38,7 +38,7 @@ public abstract class Blink1
    * @param buffer  byte buffer of correct length for report (e.g. 8 for blink1)
    *          with no reportId as the first byte 
    * @param reportId the reportId to send the buffer on (usually 1 for blink1)
-   * @returns < 0 on error, or number of bytes received
+   * @return < 0 on error, or number of bytes received
    */
   public abstract int sendFeatureReport(byte[] buffer, byte reportId);
   
@@ -46,7 +46,7 @@ public abstract class Blink1
    * Get a USB HID feature report to blink(1). Implemented by subclass
    * @param buffer the byte buffer to write into, must be correct length for report
    * @param reportId the reportId to get on
-   * @returns < 0 on error, or number of bytes received
+   * @return < 0 on error, or number of bytes received
    */
   public abstract int getFeatureReport(byte[] buffer, byte reportId);
   
@@ -57,12 +57,11 @@ public abstract class Blink1
    * @param r red component 0-255
    * @param g green component 0-255
    * @param b blue component 0-255
-   * @returns blink1_command response code, -1 == fail 
+   * @return blink1_command response code, -1 == fail
    */
   public int setRGB(int r, int g, int b) {
     byte[] buff = { (byte)'n', (byte)r, (byte)g, (byte)b, 0, 0, 0, 0};
-    int rc = this.sendFeatureReport(buff, reportId);
-    return rc;
+    return this.sendFeatureReport(buff, reportId);
   }
 
   /**
@@ -70,6 +69,7 @@ public abstract class Blink1
    */
   public int off() {
     int rc = this.stop();
+    if( rc < 0 ) return rc;
     return this.fadeToRGB(300, 0,0,0);
   }
   
@@ -77,7 +77,7 @@ public abstract class Blink1
    * Set blink(1) color immediately.
    *
    * @param c Color to set
-   * @returns blink1_command response code, -1 == fail 
+   * @return blink1_command response code, -1 == fail
    */
   public int setColor(Color c) {
     return this.setRGB( c.getRed(), c.getGreen(), c.getBlue() );
@@ -103,6 +103,7 @@ public abstract class Blink1
     int rc;
     byte[] buff = { (byte)'r', 0,0,0, 0,0,(byte)ledn,0};
     rc = this.sendFeatureReport(buff,reportId);
+    if( rc < 0 ) { return null;  }
     rc = this.getFeatureReport(buff, reportId);
     //System.out.println("BLINK1:getColor:"+Arrays.toString((buff)));
     if( rc < 0 ) { return null;  }
@@ -110,8 +111,7 @@ public abstract class Blink1
     int g = buff[3] & 0xff;
     int b = buff[4] & 0xff;
     
-    Color c = new Color( r,g,b );
-    return c;
+    return new Color( r,g,b );
   }
 
   /**
@@ -121,7 +121,7 @@ public abstract class Blink1
    * @param r red component 0-255
    * @param g green component 0-255
    * @param b blue component 0-255
-   * @returns blink1_command response code, -1 == fail 
+   * @return blink1_command response code, -1 == fail
    */
   public int fadeToRGB(int fadeMillis, int r, int g, int b ) {
     return this.fadeToRGB(fadeMillis,r,g,b,0);
@@ -135,15 +135,14 @@ public abstract class Blink1
    * @param g green component 0-255
    * @param b blue component 0-255
    * @param ledn which LED to address (0=all)
-   * @returns blink1_command response code, -1 == fail 
+   * @return blink1_command response code, -1 == fail
    */
   public int fadeToRGB(int fadeMillis, int r, int g, int b, int ledn) {
     int dms = fadeMillis/10;
     byte th = (byte)(dms >> 8);
     byte tl = (byte)(dms & 0xff);
     byte[] buff = { (byte)'c', (byte)r, (byte)g, (byte)b, th,tl, (byte)ledn, 0};
-    int rc = this.sendFeatureReport(buff, reportId);
-    return rc;
+    return this.sendFeatureReport(buff, reportId);
   }
   
   /**
@@ -151,7 +150,7 @@ public abstract class Blink1
    *
    * @param fadeMillis milliseconds to take to get to color
    * @param c Color to set
-   * @returns blink1_command response code, -1 == fail 
+   * @return blink1_command response code, -1 == fail
    */
   public int fadeToRGB(int fadeMillis, Color c) {
     return fadeToRGB( fadeMillis, c.getRed(), c.getGreen(), c.getBlue() );
@@ -163,7 +162,7 @@ public abstract class Blink1
    * @param fadeMillis milliseconds to take to get to color
    * @param c Color to set
    * @param ledn which LED to address (0=all)
-   * @returns blink1_command response code, -1 == fail 
+   * @return blink1_command response code, -1 == fail
    */
   public int fadeToRGB(int fadeMillis, Color c, int ledn) {
     return fadeToRGB( fadeMillis, c.getRed(), c.getGreen(), c.getBlue(), ledn );
@@ -174,7 +173,7 @@ public abstract class Blink1
    * @param start pattern line to start from
    * @param end pattern line to end at
    * @param count number of times to play, 0=play forever
-   * @returns blink1_command response code, -1 == fail 
+   * @return blink1_command response code, -1 == fail
    */
   public int play( int start, int end, int count ) {
     byte[] buf = {(byte)'p', 1, (byte)start, (byte)end, (byte)count, 0, 0, 0};
@@ -203,7 +202,7 @@ public abstract class Blink1
    * @param stayLit true/false to stay on when serverdown re-enabled or go off
    * @param start pattern line to start from
    * @param end pattern line to end at
-   * @returns blink1_command response code, -1 == fail 
+   * @return blink1_command response code, -1 == fail
    */
   public int serverdown( boolean enable, int millis,
                          boolean stayLit, int start, int end) {
@@ -235,7 +234,7 @@ public abstract class Blink1
   /** 
    * Get version of firmware code in blink(1) device.
    *
-   * @returns blink1 version number as int (e.g. v1.0 == 100, v2.0 = 200)
+   * @return blink1 version number as int (e.g. v1.0 == 100, v2.0 = 200)
    */
   public int getFirmwareVersion() {
     byte[] buff = { 'v', 0,0,0,0,0,0,0 };
@@ -244,8 +243,7 @@ public abstract class Blink1
     //System.out.println("BLINK1:getVersion:"+Arrays.toString((buff)));
     int vh = Character.getNumericValue(buff[3]);
     int vl = Character.getNumericValue(buff[4]);
-    int ver = (vh*100) + vl;
-    return ver;
+    return (vh*100) + vl;
   }
 
   /**
@@ -266,7 +264,7 @@ public abstract class Blink1
    * @param g green component 0-255
    * @param b blue component 0-255
    * @param pos entry position 0-patt_max
-   * @returns blink1_command response code, -1 == fail 
+   * @return blink1_command response code, -1 == fail
    */
   public int writePatternLine(int fadeMillis, int r, int g, int b, int ledn, int pos) {
     this.checkLed(ledn);
@@ -291,7 +289,7 @@ public abstract class Blink1
    * @param fadeMillis milliseconds to take to get to color
    * @param c Color to set
    * @param pos entry position 0-patt_max
-   * @returns < 0 if error
+   * @return < 0 if error
    */
   public int writePatternLine(int fadeMillis, Color c, int pos) {
     return writePatternLine(fadeMillis, c.getRed(), c.getGreen(), c.getBlue(), 0, pos);
@@ -309,7 +307,7 @@ public abstract class Blink1
    * Alters blink(1) internal pattern space but doesn't save the pattern to flash
    * @param patternlines, a List of PatternLines
    * @param clear, true = zero-out any unused patternlines, false = keep unused
-   * @returns < 0 if error
+   * @return < 0 if error
    */
   public int writePattern(PatternLine[] patternlines, boolean clear) {
     // FIXME: add clear logic
@@ -335,6 +333,7 @@ public abstract class Blink1
     int rc;
     byte[] buf = {(byte)'R', 0, 0, 0, 0, 0, (byte)pos, 0};
     rc = this.sendFeatureReport(buf,reportId);
+    if( rc < 0 ) return null;
     rc = this.getFeatureReport(buf,reportId); // FIXME: check return code
     //System.out.println("BLINK1:readPatternline:"+Arrays.toString((buf)));
     if( rc < 0 ) { return null; }
@@ -346,9 +345,7 @@ public abstract class Blink1
     int b = buf[4] & 0xff;
     int ledn = 0;
 
-    PatternLine pattline = new PatternLine(fadeMillis, r,g,b, ledn); 
-    
-    return pattline;
+    return new PatternLine(fadeMillis, r,g,b, ledn);
   }
 
   /**
@@ -377,19 +374,19 @@ public abstract class Blink1
    * Alters blink(1) internal pattern space but doesn't save the pattern to flash
    * @param patternlines, a List of PatternLines
    * @param clear, true = zero-out any unused patternlines, false = keep unused
-   * @returns -1 if error, > 0 if success
+   * @return -1 if error, > 0 if success
    */
   public int playPattern(PatternLine[] patternlines, boolean clear) {
     int rc = this.writePattern(patternlines, clear);
     if(rc<0) return rc;
-    return this.play(0,patternlines.length,Integer.MAX_VALUE);
+    return this.play(0,patternlines.length,0 /* implies play forever */);
   }
   
   /**
    * Play a given list of PatternLines as a pattern
    * Alters blink(1) internal pattern space but doesn't save the pattern to flash
    * @param patternlines, a List of PatternLines
-   * @returns -1 if error, > 0 if success
+   * @return -1 if error, > 0 if success
    */
   public int playPattern(PatternLine[] patternlines) {
     return this.playPattern(patternlines,true);
@@ -410,7 +407,7 @@ public abstract class Blink1
   /**
    * one attempt at a degamma curve.
    */
-  public static final int log2lin( int n ) {
+  public static int log2lin(int n ) {
     //return  (int)(1.0* (n * 0.707 ));  // 1/sqrt(2)
     return (((1<<(n/32))-1) + ((1<<(n/32))*((n%32)+1)+15)/32);
   }
@@ -418,10 +415,10 @@ public abstract class Blink1
   /**
    * Utility: A simple delay
    */
-  public static final void pause(int millis) {
+  public static void pause(int millis) {
     try {
         Thread.sleep(millis);
-    } catch (Exception e) {
+    } catch (Exception ignored) {
     }
   }
 
